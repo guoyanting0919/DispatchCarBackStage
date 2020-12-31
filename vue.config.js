e730e57bedd101771499344c5d236f2b0565ec8c
YT <guoyanting0919@gmail.com>
2020-12-31 15:14:42 +0800

    檔案優化

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 function resolve(dir) {
   return path.join(__dirname, "/", dir);
 }
+// const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = {
   publicPath: "/dist/",
@@ -25,9 +26,42 @@ module.exports = {
     },
   },
 
-  configureWebpack: {
-    devtool: "source-map",
+  // configureWebpack: {
+  //   devtool: "source-map",
+  //   // plugins: [new BundleAnalyzerPlugin()],
+
+  //   // externals: {
+  //   //   vue: "Vue",
+  //   //   vuex: "Vuex",
+  //   //   axios: "axios",
+  //   // },
+  // },
+
+  configureWebpack: (config) => {
+    config.optimization = {
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 20000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace("@", "")}`;
+            },
+          },
+        },
+      },
+    };
   },
+
   // svg配置
   chainWebpack(config) {
     config.module
