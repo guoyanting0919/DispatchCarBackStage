5c2c33289edfb55538eb4682060c186df5ee690b
YT <guoyanting0919@gmail.com>
2021-02-22 18:19:41 +0800

    revenusreport change top chart

--- src/views/revenuereport/index.vue
+++ src/views/revenuereport/index.vue
@@ -3,44 +3,17 @@
     <sticky :className="'sub-navbar'">
       <div class="filter-container">
         <!-- 區域選擇 -->
-        <el-select
-          size="mini"
-          v-model="value"
-          clearable
-          placeholder="請選擇區域"
-        >
-          <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          ></el-option>
+        <el-select size="mini" v-model="value" clearable placeholder="請選擇區域">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
         </el-select>
 
         <!-- 公司選擇 -->
-        <el-select
-          size="mini"
-          v-model="value"
-          clearable
-          placeholder="請選擇公司"
-        >
-          <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          ></el-option>
+        <el-select size="mini" v-model="value" clearable placeholder="請選擇公司">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
         </el-select>
 
         <!-- 日期選擇 -->
-        <el-date-picker
-          size="mini"
-          v-model="value1"
-          type="daterange"
-          range-separator="至"
-          start-placeholder="開始日期"
-          end-placeholder="结束日期"
-        ></el-date-picker>
+        <el-date-picker size="mini" v-model="value1" type="daterange" range-separator="至" start-placeholder="開始日期" end-placeholder="结束日期"></el-date-picker>
       </div>
     </sticky>
 
@@ -48,10 +21,7 @@
       <!-- 營收報表 -->
       <div class="reportContainer">
         <Title title="營收報表"></Title>
-        <ve-histogram
-          :data="chartData"
-          :settings="chartSettings"
-        ></ve-histogram>
+        <ve-bar height="300px" :data="chartData"></ve-bar>
       </div>
 
       <!-- 車輛使用率 -->
@@ -61,11 +31,7 @@
           <div class="carBox" v-for="item in 5" :key="item">
             <p>NAM-03</p>
             <p>豐均國際租賃有限公司</p>
-            <ve-ring
-              class="carChart"
-              :data="chartData2"
-              :settings="chartSettings2"
-            ></ve-ring>
+            <ve-ring class="carChart" :data="chartData2" :settings="chartSettings2"></ve-ring>
           </div>
         </div>
       </div>
@@ -83,18 +49,39 @@ export default {
     Title,
   },
   data() {
-    this.chartSettings = {
-      metrics: ["金額"],
-      dimension: ["車牌"],
-      yAxisName: ["單位(NTD)"],
-    };
-    this.chartSettings2 = {
-      radius: [40, 80],
-      offsetY: 120,
-      dimension: "日期",
-      metrics: "访问用户",
-    };
+    // this.chartSettings = {
+    //   metrics: ["金額"],
+    //   dimension: ["車牌"],
+    //   yAxisName: ["單位(NTD)"],
+    // };
     return {
+      chartSettings: {
+        metrics: ["金額"],
+        dimension: ["key"],
+      },
+      chartData: {
+        columns: ["key", "金額"],
+        rows: [
+          { key: "總營收", 金額: 654614 },
+          { key: "個案負擔", 金額: 126768 },
+          { key: "政府補助", 金額: 491846 },
+        ],
+      },
+      chartSettings2: {
+        radius: [40, 80],
+        offsetY: 120,
+        dimension: "key",
+        metrics: "value",
+      },
+
+      chartData2: {
+        columns: ["key", "value"],
+        rows: [
+          { key: "個案負擔", value: 30 },
+          { key: "政府補助", value: 70 },
+          { key: "總營收", value: 70 },
+        ],
+      },
       value: "",
       value1: "",
       options: [
@@ -119,21 +106,6 @@ export default {
           label: "北京烤鸭",
         },
       ],
-      chartData: {
-        columns: ["車牌", "金額"],
-        rows: [
-          { 車牌: "總營收", 金額: 654614 },
-          { 車牌: "個案負擔", 金額: 126768 },
-          { 車牌: "政府補助", 金額: 491846 },
-        ],
-      },
-      chartData2: {
-        columns: ["日期", "访问用户"],
-        rows: [
-          { 日期: "使用率", 访问用户: 30 },
-          { 日期: "未使用率", 访问用户: 70 },
-        ],
-      },
     };
   },
 };
