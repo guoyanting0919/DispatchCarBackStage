4c96cfd1ec86a05a9fb0d2958ed63dc522017816
YT <guoyanting0919@gmail.com>
2021-02-25 13:58:51 +0800

    poly line map in dashboard page

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -14,14 +14,18 @@
       <el-button @click="changeStatus(4)" type="success">客上</el-button>
       <el-button @click="changeStatus(5),orderPay" type="info">收款 + 完成</el-button>
       <el-button @click="changeStatus(9,'SYS_ORDERCANCEL_REMARK_DRIVER')" type="danger">取消</el-button>
-      <div class="resMessage">
-
-        {{resMessage}}
-      </div>
+      <div class="resMessage">{{resMessage}}</div>
     </div>
+
+    <!-- polyline map -->
+    <div ref="map" id='map' style="width:500px;height:500px"></div>
   </div>
 </template>
-
+  <script
+      async
+      defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCO-vJiy_0WsK6RB9ucxTR98zdIB6f7AxI&libraries=places&language=zh-TW&region=TW"
+    ></script>
 <script>
 import * as driverApp from "@/api/driverApp";
 
@@ -30,6 +34,7 @@ export default {
     return {
       despatchNo: "",
       resMessage: "RES MESSAGE",
+      map: null,
     };
   },
   computed: {
@@ -38,6 +43,38 @@ export default {
     },
   },
   methods: {
+    /* init map */
+    initMap() {
+      const vm = this;
+      vm.map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: {
+          lat: 25.0336962,
+          lng: 121.5643673,
+        },
+      });
+
+      var polylinePathPoints = [
+        { lat: 25.0336962, lng: 121.5643673 },
+        { lat: 25.033755, lng: 121.565412 },
+        { lat: 25.031985, lng: 121.56538 },
+        { lat: 25.032083, lng: 121.561324 },
+      ];
+
+      var polylinePath = new google.maps.Polyline({
+        path: polylinePathPoints,
+        geodesic: true,
+        strokeColor: "#008800",
+        strokeOpacity: 0.8,
+        strokeWeight: 20,
+        editable: true,
+        geodesic: false,
+        draggable: false,
+      });
+
+      polylinePath.setMap(vm.map);
+    },
+
     /* 變更訂單狀態 */
     changeStatus(status, cancelRemark = "") {
       const vm = this;
@@ -74,7 +111,9 @@ export default {
       });
     },
   },
-
+  mounted() {
+    this.initMap();
+  },
   created() {},
 };
 </script>
